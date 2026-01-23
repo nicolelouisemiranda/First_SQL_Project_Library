b3b71b67d175cf4dce05a052471b95e21390ea29
Nicole Miranda <145497306+nicolelouisemiranda@users.noreply.github.com>
2026-01-23 20:19:28 -0300

    Enhance SQL script with genre grouping and updates
    Added SQL queries to group books by genre and fix genre formatting.

--- script_explore_db.sql
+++ script_explore_db.sql
@@ -41,3 +41,41 @@ SELECT
 FROM authors
 GROUP BY country
 ORDER BY number_of_authors DESC;
+
+-- Group books by genre
+SELECT 
+	genre,
+	count(title) AS number_of_books
+FROM books
+GROUP BY genre
+ORDER BY number_of_books DESC;
+
+-- Fixing the issue of formatting with the science-fiction genre
+UPDATE books 
+SET genre = 'science-fiction'
+WHERE genre = 'science fiction';
+
+-- Testing results when grouping books by genre
+SELECT 
+	genre,
+	count(title) AS number_of_books
+FROM books
+GROUP BY genre
+ORDER BY number_of_books DESC;
+
+-- Did I read more than one book from any of the authors?
+SELECT 
+	title,
+	author_id
+FROM books 
+WHERE author_id IN (SELECT author_id FROM books GROUP BY author_id HAVING count(author_id)>1);
+
+-- Did I read more than one book from any of the authors?
+SELECT 
+    b.title,
+    a.author
+FROM books b
+JOIN authors a
+    ON a.author_id = b.author_id
+WHERE b.author_id IN (SELECT author_id FROM books GROUP BY author_id HAVING COUNT(author_id) > 1)
+ORDER BY a.author DESC;
